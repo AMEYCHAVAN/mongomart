56e5b65aec7060924d14d2cb422cbff4b8f95dce
AChavan <amey.chavan@b2x.com>
2016-02-25 18:06:06 +0530

    lab 5 finished hurray

--- cart.js
+++ cart.js
@@ -24,29 +24,47 @@ function CartDAO(database) {
 
     this.db = database;
 
-    
+
     this.getCart = function(userId, callback) {
         "use strict";
-        
-        /*
-        * TODO-lab5
-        *
-        * LAB #5: 
-        * Query the "cart" collection by userId and pass the cart to the 
-        * callback function.
-        *
-        */
 
-        var userCart = {
-            userId: userId,
-            items: []
-        }
-        var dummyItem = this.createDummyItem();
-        userCart.items.push(dummyItem);
-        
-        // TODO-lab5 Replace all code above (in this method).
 
-        callback(userCart);
+
+        console.log("\n\n8this.getCart", userId )
+
+
+         var query = {"userId":  userId }
+
+         console.log("8this.getItems","resultquery", query)
+
+         this.db.collection('cart').find(query).toArray(function(err, result) {
+           assert.equal(err, null);
+           console.log("8this.getItems","resultlength", result)
+           var result = result.pop()
+
+           var userCart = {
+              userId: userId,
+              items: result.items
+          }
+
+           callback(userCart);
+
+
+
+         });
+
+
+
+        // var userCart = {
+        //     userId: userId,
+        //     items: []
+        // }
+        // var dummyItem = this.createDummyItem();
+        // userCart.items.push(dummyItem);
+        //
+        // // TODO-lab5 Replace all code above (in this method).
+        //
+        // callback(userCart);
     }
 
 
@@ -56,25 +74,25 @@ function CartDAO(database) {
         /*
          *
          * TODO-lab6
-         * 
+         *
          * LAB: #6
          *
-         * Write a query that will determine whether or not the cart associated 
-         * with the userId contains an item identified by itemId. If the cart 
-         * does contain the item, pass the item to the callback. If it does not, 
-         * pass the value null to the callback. 
+         * Write a query that will determine whether or not the cart associated
+         * with the userId contains an item identified by itemId. If the cart
+         * does contain the item, pass the item to the callback. If it does not,
+         * pass the value null to the callback.
          *
-         * NOTE: You should pass only the matching item to the callback. Do not 
+         * NOTE: You should pass only the matching item to the callback. Do not
          * pass an array of one or more items or the entire cart.
          *
-         * SUGGESTION: While it is not necessary, you might find it easier to 
-         * use the $ operator in a projection document in your call to find() as 
-         * a means of selecting the matching item. Again, take care to pass only 
+         * SUGGESTION: While it is not necessary, you might find it easier to
+         * use the $ operator in a projection document in your call to find() as
+         * a means of selecting the matching item. Again, take care to pass only
          * the matching item (not an array) to the callback. See:
          * https://docs.mongodb.org/manual/reference/operator/projection/positional/
          *
-         * As context for this method to better understand its purpose, look at 
-         * how cart.itemInCart is used in the mongomart.js app. 
+         * As context for this method to better understand its purpose, look at
+         * how cart.itemInCart is used in the mongomart.js app.
          *
          */
 
@@ -83,16 +101,16 @@ function CartDAO(database) {
         // TODO-lab6 Replace all code above (in this method).
     }
 
-    
+
     /*
      * This solution is provide as an example to you of several query
-     * language features that are valuable in update operations. 
-     * This method adds the item document passed in the item parameter to the 
+     * language features that are valuable in update operations.
+     * This method adds the item document passed in the item parameter to the
      * user's cart. Note that this solution works regardless of whether the
-     * cart already contains items or is empty. addItem will be called only 
+     * cart already contains items or is empty. addItem will be called only
      * if the cart does not already contain the item. The route handler:
      * router.post("/user/:userId/cart/items/:itemId"...
-     * handles this. Please review how that method works to have a complete 
+     * handles this. Please review how that method works to have a complete
      * understanding of how addItem is used.
      *
      * NOTE: One may use either updateOne() or findOneAndUpdate() to
@@ -146,25 +164,25 @@ function CartDAO(database) {
                 callback(result.value);
             });
         */
-        
+
     };
 
 
     this.updateQuantity = function(userId, itemId, quantity, callback) {
         "use strict";
-        
+
         /*
         * TODO-lab7
         *
-        * LAB #7: Update the quantity of an item in the user's cart in the 
-        * database by setting quantity to the value passed in the quantity 
-        * parameter. If the value passed for quantity is 0, remove the item 
+        * LAB #7: Update the quantity of an item in the user's cart in the
+        * database by setting quantity to the value passed in the quantity
+        * parameter. If the value passed for quantity is 0, remove the item
         * from the user's cart stored in the database.
         *
         * Pass the updated user's cart to the callback.
         *
         * NOTE: Use the solution for addItem as a guide to your solution for
-        * this problem. There are several ways to solve this. By far, the 
+        * this problem. There are several ways to solve this. By far, the
         * easiest is to use the $ operator. See:
         * https://docs.mongodb.org/manual/reference/operator/update/positional/
         *
@@ -178,7 +196,7 @@ function CartDAO(database) {
         dummyItem.quantity = quantity;
         userCart.items.push(dummyItem);
         callback(userCart);
-        
+
         // TODO-lab7 Replace all code above (in this method).
 
     }
